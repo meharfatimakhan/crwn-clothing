5d0fcfc63cd458c70aa1d8a0628cfcaa4c5006dd
Mehar Fatima Khan <meharfatimak@gmail.com>
2020-07-27 15:41:27 +0500

    Added Header Component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import './App.css';
 import  HomePage  from './pages/homepage/homepage.component';
+import  ShopPage  from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import { Switch, Route } from 'react-router-dom';
 
-const HatsPage = () => {
-  return (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-  );
+// const HatsPage = () => {
+//   return (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+//   );
   
-}
+// }
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
-        <Route path='/shop/hats' component={HatsPage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
   );
