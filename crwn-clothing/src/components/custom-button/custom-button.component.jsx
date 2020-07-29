6be6b2c77fab748b3b00f86224847f9a29e3589b
Mehar Fatima Khan <meharfatimak@gmail.com>
2020-07-29 23:11:30 +0500

    Revert Commits

---
+++ crwn-clothing/src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './custom-button.style.scss';
+
+const CustomButton = ({children, isGoogleSignIn,inverted,...otherProps}) => (
+    <button className={`custom-button ${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''}`} {...otherProps}>
+        {children}
+    </button>
+)
+
+export default CustomButton;
