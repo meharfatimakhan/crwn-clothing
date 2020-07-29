fd84ffd125eb4547a749cc43344d3e1d7376a274
meharfatimakhan <l164388@lhr.nu.edu.pk>
2020-07-29 16:28:40 +0500

    Updated shop.data with new shop.data

--- crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -8,8 +8,8 @@ import CollectionItem from '../collection-item/collection-item.component';
          <h1 className='title'>{title.toUpperCase()}</h1>
          <div className='preview'>
              {
-                 items.filter((item, idx) => idx < 4).map(({id, ...otherItemProps}) => (
-                  <CollectionItem key={id} {...otherItemProps}/>
+                 items.filter((item, idx) => idx < 4).map(item => (
+                  <CollectionItem key={item.id} item={item}/>
                  ))
              }
          </div>
